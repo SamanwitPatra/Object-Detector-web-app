cec8754c3294af07fc61ae95d6d192d64b0e4498
SamanwitPatra <94913225+SamanwitPatra@users.noreply.github.com>
2023-06-18 12:48:09 +0530

    Add files via upload

--- fruit_basket.js
+++ fruit_basket.js
@@ -2,5 +2,25 @@ status="";
 
 function preload()
 {
-    
+    loadImage("fruit basket.jpeg");
+}
+function setup() 
+{
+    createCanvas= canvas(350, 500);
+    objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status = Detecting Objects"
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status= true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, gotResults)
+{
+     if(error) 
+     {
+        console.log(error);
+     }
+     console.log(results);
 }
